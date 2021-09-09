d71175654fd9e3f42e65cd857d174df6608ac204
Wesley Sanchez <77991913+SanWes@users.noreply.github.com>
2021-09-09 01:15:44 -0400

    Crypto currency app. Calling API by using Axios
    Crypto currency app. Using coingecko API to display top cryptos.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import CoinsWithAxios from './components/CoinsWithAxios';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Top Cryptos Today!</h1>
+      {/* <Coins></Coins> */}
+      <CoinsWithAxios></CoinsWithAxios>
     </div>
   );
 }
